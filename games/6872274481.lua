34dbad19bdf091409ed95196737bd9a2c308fc18
CookieScript <xairery@gmail.com>
2026-09-06 17:59:56 +0800

    Update 6872274481.lua

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -30,10 +30,11 @@ end
 local gameCamera = workspace.CurrentCamera
 local lplr = playersService.LocalPlayer
 
+--[[ Vape v4 Reborrn ❤️❤️❤️
 local kickThread = task.spawn(lplr.Kick, lplr, 'Bedwars is no longer supported by Vape V4, thank you for 5 years of support ❤️')
 if coroutine.status(kickThread) ~= 'dead' then
 	game:Shutdown()
-end
+end]]
 
 local vape = shared.vape
 local entitylib = vape.Libraries.entity
