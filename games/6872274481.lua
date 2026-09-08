2485f0dd0cfe8d3371aafa01c68307326ca99379
CookieScript <xairery@gmail.com>
2026-09-08 10:00:26 +0800

    Updated Vape

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -4409,6 +4409,24 @@ run(function()
 end)
 
 run(function()
+	local InfiniteJump
+
+	InfiniteJump = vape.Categories.Utility:CreateModule({
+		Name = 'InfiniteJump',
+		Function = function(callback)
+            if callback then
+			    InfiniteJump:Clean(inputService.JumpRequest:Connect(function()
+                    if entitylib.isAlive then
+					    entitylib.character.Humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
+					end
+				end))
+            end
+		end,
+		Tooltip = 'Allows you to jump infinitely.'
+	})
+end)
+
+run(function()
 	local AutoToxic
 	local GG
 	local Toggles, Lists, said, dead = {}, {}, {}
