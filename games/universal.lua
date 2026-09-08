8d98fa403924f8a98c64623d5d82e58e1fbff2ab
CookieScript <xairery@gmail.com>
2026-09-08 08:46:36 +0800

    Updated Vape

--- games/universal.lua
+++ games/universal.lua
@@ -260,6 +260,10 @@ vape.Libraries.auraanims = {
 		{CFrame = CFrame.new(-0.62, -0.68, -0.07) * CFrame.Angles(math.rad(-167), math.rad(47), math.rad(-1)), Time = 0.03},
 		{CFrame = CFrame.new(-0.56, -0.86, 0.23) * CFrame.Angles(math.rad(-167), math.rad(49), math.rad(-1)), Time = 0.03}
 	},
+	['Classic'] = {
+		{CFrame = CFrame.Angles(math.rad(125), math.rad(125), math.rad(145)), Time = 0.12},
+		{CFrame = CFrame.Angles(math.rad(28), math.rad(125), math.rad(195)), Time = 0.12},
+	},
 	['Random'] = {},
 	['Horizontal Spin'] = {
 		{CFrame = CFrame.Angles(math.rad(-10), math.rad(-90), math.rad(-80)), Time = 0.12},
@@ -283,7 +287,7 @@ vape.Libraries.auraanims = {
 		{CFrame = CFrame.new(0.7, -0.71, 0.59) * CFrame.Angles(math.rad(-84), math.rad(50), math.rad(-38)), Time = 0.1},
 		{CFrame = CFrame.new(0.7, -0.71, 0.59) * CFrame.Angles(math.rad(-84), math.rad(50), math.rad(-38)), Time = 0.05},
 		{CFrame = CFrame.new(0.63, -0.1, 1.37) * CFrame.Angles(math.rad(-84), math.rad(50), math.rad(-38)), Time = 0.15}
-	}
+	},
 }
 
 local SpeedMethods
