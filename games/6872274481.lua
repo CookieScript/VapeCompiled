ed6a795c924080376bdc150d28f5169d79d43d16
CookieScript <xairery@gmail.com>
2026-09-14 16:38:11 +0800

    Updated Vape

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -1798,7 +1798,8 @@ run(function()
 						bedwars.BalloonController:inflateBalloon()
 					end
 				end))
-				Fly:Clean(runService.PreSimulation:Connect(function(dt)
+				-- // PreSimulation was the old one
+				Fly:Clean(runService.Heartbeat:Connect(function(dt)
 					if entitylib.isAlive and playersService.LocalPlayer.Character and playersService.LocalPlayer.Character:FindFirstChild('HumanoidRootPart') and not InfiniteFly.Enabled and isnetworkowner(entitylib.character.RootPart) then
 						local flyAllowed = (lplr.Character:GetAttribute('InflatedBalloons') and lplr.Character:GetAttribute('InflatedBalloons') > 0) or store.matchState == 2
 						local mass = (1.5 + (flyAllowed and 6 or 0) * (tick() % 0.4 < 0.2 and -1 or 1)) + ((up + down) * VerticalValue.Value)
