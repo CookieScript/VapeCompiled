6d606a37d4d33303864159daf3037f526715a46b
CookieScript <xairery@gmail.com>
2026-09-16 10:39:25 +0800

    Updated Vape

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -1652,7 +1652,7 @@ run(function()
 									local lastTeleport = lplr:GetAttribute('LastTeleported')
 									local connection
 									connection = runService.PreSimulation:Connect(function()
-										if vape.Modules.Fly.Enabled or (vape.Modules.InfiniteFly and vape.Modules.InfiniteFly.Enabled) or vape.Modules.LongJump.Enabled then
+										if vape.Modules.Fly.Enabled or vape.Modules.LongJump.Enabled then
 											connection:Disconnect()
 											AntiFallDirection = nil
 											return
@@ -1793,7 +1793,6 @@ run(function()
 end)
 
 local Fly
-local InfiniteFly
 local LongJump
 run(function()
 	local Value
@@ -2041,108 +2040,6 @@ run(function()
 		Suffix = function(val)
 			return val == 1 and 'stud' or 'studs'
 		end
-	})
-end)
-
-run(function()
-	local Value
-	local VerticalValue
-	local WallCheck
-	local rayCheck = RaycastParams.new()
-	rayCheck.RespectCanCollide = true
-	local up, down = 0, 0
-
-	InfiniteFly = vape.Categories.Blatant:CreateModule({
-		Name = 'InfiniteFly',
-		Function = function(callback)
-			frictionTable.InfiniteFly = callback or nil
-			updateVelocity()
-			if callback then
-				up, down = 0, 0
-				InfiniteFly:Clean(runService.Heartbeat:Connect(function(dt)
-					if entitylib.isAlive and lplr.Character and lplr.Character:FindFirstChild('HumanoidRootPart') and isnetworkowner(entitylib.character.RootPart) then
-						local root, moveDirection = entitylib.character.RootPart, entitylib.character.Humanoid.MoveDirection
-						local velo = getSpeed()
-						local oldvelo = lplr.Character.HumanoidRootPart.Velocity
-						local oldcframe = lplr.Character.HumanoidRootPart.CFrame
-						local destination = (moveDirection * math.max(Value.Value - velo, 0) * dt)
-						rayCheck.FilterDescendantsInstances = {lplr.Character, gameCamera, AntiFallPart}
-						rayCheck.CollisionGroup = root.CollisionGroup
-
-						if WallCheck.Enabled then
-							local ray = workspace:Raycast(root.Position, destination, rayCheck)
-							if ray then
-								destination = ((ray.Position + ray.Normal) - root.Position)
-							end
-						end
-
-						root.CFrame = CFrame.new(Vector3.new(root.Position.X, 100, root.Position.Z) + destination)
-						root.AssemblyLinearVelocity = (moveDirection * velo) + Vector3.new(0, (up + down) * VerticalValue.Value, 0)
-
-						runService:BindToRenderStep('InfiniteFlySpoofing', 199, function()
-							lplr.Character.HumanoidRootPart.CFrame = oldcframe
-					        lplr.Character.HumanoidRootPart.Velocity = Vector3.new(oldvelo.X, lplr.Character.HumanoidRootPart.Velocity.Y, oldvelo.Z)
-						    runService:UnbindFromRenderStep('InfiniteFlySpoofing')
-						end)
-					end
-				end))
-				InfiniteFly:Clean(inputService.InputBegan:Connect(function(input)
-					if not inputService:GetFocusedTextBox() then
-						if input.KeyCode == Enum.KeyCode.Space or input.KeyCode == Enum.KeyCode.ButtonA then
-							up = 1
-						elseif input.KeyCode == Enum.KeyCode.LeftShift or input.KeyCode == Enum.KeyCode.ButtonL2 then
-							down = -1
-						end
-					end
-				end))
-				InfiniteFly:Clean(inputService.InputEnded:Connect(function(input)
-					if input.KeyCode == Enum.KeyCode.Space or input.KeyCode == Enum.KeyCode.ButtonA then
-						up = 0
-					elseif input.KeyCode == Enum.KeyCode.LeftShift or input.KeyCode == Enum.KeyCode.ButtonL2 then
-						down = 0
-					end
-				end))
-				if inputService.TouchEnabled then
-					pcall(function()
-						local jumpButton = lplr.PlayerGui.TouchGui.TouchControlFrame.JumpButton
-						InfiniteFly:Clean(jumpButton:GetPropertyChangedSignal('ImageRectOffset'):Connect(function()
-							up = jumpButton.ImageRectOffset.X == 146 and 1 or 0
-						end))
-					end)
-				end
-			else
-				lplr.Character.HumanoidRootPart.Velocity = Vector3.new(0, 50, 0)
-				entitylib.character.Humanoid:ChangeState(Enum.HumanoidStateType.Landed)
-				lplr.Character.HumanoidRootPart.CFrame = CFrame.new(lplr.Character.HumanoidRootPart.CFrame.Position.X, 100, lplr.Character.HumanoidRootPart.CFrame.Position.Z)
-				notif('InfiniteFly', 'Landed', 1.5)
-			end
-		end,
-		ExtraText = function()
-			return 'Infinite'
-		end,
-		Tooltip = 'Makes you go zoom.'
-	})
-	Value = InfiniteFly:CreateSlider({
-		Name = 'Speed',
-		Min = 1,
-		Max = 23,
-		Default = 20,
-		Suffix = function(val)
-			return val == 1 and 'stud' or 'studs'
-		end
-	})
-	VerticalValue = InfiniteFly:CreateSlider({
-		Name = 'Vertical Speed',
-		Min = 1,
-		Max = 150,
-		Default = 50,
-		Suffix = function(val)
-			return val == 1 and 'stud' or 'studs'
-		end
-	})
-	WallCheck = InfiniteFly:CreateToggle({
-		Name = 'Wall Check',
-		Default = true
 	})
 end)
 
