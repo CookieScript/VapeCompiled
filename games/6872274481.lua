20a2d0ae850a35e2c6f8dcf864e8a0855dd51531
CookieScript <xairery@gmail.com>
2026-09-20 19:29:40 +0800

    Updated Vape

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -1743,6 +1743,48 @@ run(function()
 end)
 
 run(function()
+	local AntiHit
+
+	local function IsTouchingFloor()
+		if entitylib.isAlive then
+			return entitylib.character.Humanoid.FloorMaterial ~= Enum.Material.Air
+		end
+
+		return false
+	end
+
+	AntiHit = vape.Categories.Blatant:CreateModule({
+		Name = 'AntiHit',
+		Function = function(callback)
+			if callback then
+				AntiHit:Clean(runService.Heartbeat:Connect(function()
+					if entitylib.isAlive and IsTouchingFloor() then
+						local closestEntity = entitylib.EntityPosition({
+							Range = 14.4,
+							Part = 'RootPart',
+							Players = true,
+							NPCs = false,
+							Wallcheck = false,
+							Sort = function(a, b)
+								local selfPos = entitylib.character.RootPart.Position
+								return (a.RootPart.Position - selfPos).Magnitude < (b.RootPart.Position - selfPos).Magnitude
+							end
+						})
+
+						if closestEntity then
+							local root = entitylib.character.RootPart
+							root.CFrame = root.CFrame + Vector3.new(0, 38, 0)
+							root.AssemblyLinearVelocity = Vector3.new(0, -300, 0)
+						end
+					end
+				end))
+			end
+		end,
+		Tooltip = 'Dodges hits from other players'
+	})
+end)
+
+run(function()
 	local DamageBoost
     local cooldown
 
