084add19d64c6db04edb72d4e5c4d983165b5427
CookieScript <xairery@gmail.com>
2026-09-26 15:39:55 +0800

    Updated Vape

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -4045,6 +4045,7 @@ run(function()
 	})
 	List = StorageESP:CreateTextList({
 		Name = 'Item',
+		Default = { 'land', 'grass', 'wool', 'obsidian', 'ice', 'wood', 'iron', 'diamond', 'emerald', 'sword', 'rage', 'arrow', 'fire', 'telepearl', 'bow', 'apple', 'potion', 'pie', 'balloon' },
 		Function = function()
 			for _, v in Reference do
 				task.spawn(refreshAdornee, v)
